10d5afe117bd9474ff0e3e62007910a038e5546c
wamah <pythondeveloper21@gmail.com>
2023-03-06 20:18:28 +0000

    update on image inserting

--- inserting_images.py
+++ inserting_images.py
@@ -15,3 +15,29 @@ cur.execute("INSERT INTO Images VALUES(1, %s)", (show_photo,))
 
 con.commit()
 print('Image Inserted successfully')
+
+
+#YOU CAN ALSO TRY THIS CODE
+
+
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='yourpassword',
+                               host='localhost', database='yourdatabase')
+cursor = cnx.cursor()
+
+with open('image.jpg', 'rb') as f:
+    image_data = f.read()
+
+add_image = ("INSERT INTO Images "
+             "(ID, DATA) "
+             "VALUES (%s, %s)")
+image_id = 1  # Replace with the ID of the image
+image_blob = mysql.connector.Binary(image_data)
+data = (image_id, image_blob)
+
+cursor.execute(add_image, data)
+cnx.commit()
+
+cursor.close()
+cnx.close()
